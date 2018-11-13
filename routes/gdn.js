6289c5d4928e138fdfab2343e03f7b792f745ead
Matthew C. Wake <mcwake@outlook.com>
2018-11-13 11:53:43 +0000

    Removed db from Github

--- routes/gdn.js
+++ routes/gdn.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const sqlite3 = require("sqlite3").verbose();
+
+let db = new sqlite3.Database("../data/database/gdn.db", err => {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log("Connected");
+});
 
 module.exports = router;
