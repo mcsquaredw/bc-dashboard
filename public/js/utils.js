ca1f769417fe7317a16fb3982ef6f373e9374262
Matthew C. Wake <mcwake@outlook.com>
2018-11-16 10:08:40 +0000

    Updated display of time and date

--- public/js/utils.js
+++ public/js/utils.js
@@ -23,3 +23,20 @@ function getSelectedFromSelect(select) {
     }
   }
 }
+
+function formatTime(dateStr) {
+  const dateObj = new Date(dateStr);
+
+  return `${("0" + dateObj.getHours()).slice(-2)}:${(
+    "0" + dateObj.getMinutes()
+  ).slice(-2)}:${("0" + dateObj.getSeconds()).slice(-2)}`;
+}
+
+function formatDate(dateStr) {
+  const dateObj = new Date(dateStr);
+
+  return `${("0" + dateObj.getDate()).slice(-2)}/${(
+    "0" +
+    (dateObj.getMonth() + 1)
+  ).slice(-2)}/${dateObj.getFullYear()}`;
+}
