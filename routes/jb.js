9b21353e8aa1d4f553111f34114197f865693513
Matthew C. Wake <mcwake@outlook.com>
2018-12-11 10:58:58 +0000

    Removed problematic caching mechanism

--- routes/jb.js
+++ routes/jb.js
@@ -4,34 +4,9 @@ const moment = require("moment");
 
 const router = express.Router();
 const { username, password, api_key } = require("../config");
-const cache = {};
-const FLAGS = "FLAGS";
-const JOBS = "JOBS";
-const RESOURCES = "RESOURCES";
-
-cache[FLAGS] = {};
-cache[JOBS] = {};
-cache[RESOURCES] = {};
-
-function outOfDate(then) {
-  let now = new Date().getTime();
-
-  if((now - then) > 60000) {
-    return true;
-  }
-
-  return false;
-}
-
-function updateCache(key, data) {
-  let now = new Date().getTime();
-
-  cache[key].time = now;
-  cache[key].data = Object.assign(data, cache[key].data);
-}
 
 router.get("/all-jobs", (req, res) => {
-  if(!cache[JOBS].data || outOfDate(cache[JOBS].time)) {
+
     axios
     .get(
       `https://webservice.bigchangeapps.com/v01/services.ashx?action=jobs&key=${api_key}&login=${username}&pwd=${password}&start=${moment("2018-11-01").format('YYYY-MM-DD')}&end=${moment().add(60, 'days').format('YYYY-MM-DD')}`,
@@ -42,15 +17,11 @@ router.get("/all-jobs", (req, res) => {
       }
     )
     .then(response => {
-      updateCache(JOBS, response.data.Result);
       res.send(response.data.Result);
     })
     .catch(err => {
       console.log(err);
     });
-  } else {
-    res.send(cache[JOBS].data);
-  }
 });
 
 router.get("/job/:id", (req, res) => {
@@ -74,7 +45,6 @@ router.get("/job/:id", (req, res) => {
 });
 
 router.get("/resources", (req, res) => {
-  if(!cache[RESOURCES].data || outOfDate(cache[RESOURCES].time)) {
     axios
     .get(
       `https://webservice.bigchangeapps.com/v01/services.ashx?&key=${api_key}&login=${username}&pwd=${password}&action=live`,
@@ -85,19 +55,14 @@ router.get("/resources", (req, res) => {
       }
     )
     .then(response => {
-      updateCache(RESOURCES, response.data.Result);
       res.send(response.data.Result);
     })
     .catch(err => {
       console.log(err);
     });
-  } else {
-    res.send(cache[RESOURCES].data);
-  }
 });
 
 router.get("/flags", (req, res) => {
-  if(!cache[FLAGS].data) {
     axios
     .get(
       `https://webservice.bigchangeapps.com/v01/services.ashx?&key=${api_key}&login=${username}&pwd=${password}&action=tags`,
@@ -108,15 +73,11 @@ router.get("/flags", (req, res) => {
       }
     )
     .then(response => {
-      updateCache(FLAGS, response.data.Result);
       res.send(response.data.Result);
     })
     .catch(err => {
       console.log(err);
     });
-  } else {
-    res.send(cache[FLAGS].data);
-  }
 });
 
 module.exports = router;
