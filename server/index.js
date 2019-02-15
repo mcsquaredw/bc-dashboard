0b0c9626fbaf82075598188187de9fa028a205e6
M C Wake <mcwake@outlook.com>
2019-02-15 12:10:23 +0000

    Added assertion checks for required config/environment variables

--- server/index.js
+++ server/index.js
@@ -7,6 +7,22 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 let db;
 
+assert(config.BC_API_KEY, "Big Change API Key not present");
+assert(config.BC_USERNAME, "Big Change Username not present");
+assert(config.BC_PASSWORD, "Big Change Password not present");
+assert(config.CERT, "HTTPS Certificate path not present");
+assert(config.KEY, "HTTPS Key path not present");
+assert(config.COMPANY_NAME, "Company Name not present");
+assert(config.MONGO_URL, "MongoDB URL not present");
+assert(config.MONGO_DBNAME, "MongoDB Database Name not present");
+assert(config.EPOCH, "Start Date not present");
+assert(config.EMAIL_OUTGOING_SERVER, "Email Server not present");
+assert(config.EMAIL_USERNAME, "Email Username not present");
+assert(config.EMAIL_PASSWORD, "Email Password not present");
+assert(config.BC_API_KEY, "Email Destination address(es) not present");
+assert(config.ENGINEERS, "No Engineers specified");
+assert(config.SURVEYORS, "No Surveyors specified");
+
 MongoClient.connect(config.MONGO_URL, { useNewUrlParser: true }, (err, client) => {
     assert.equal(null, err);
     db = client.db(config.MONGO_DBNAME);
